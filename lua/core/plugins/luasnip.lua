59cac1966c60d01520badd9841e32e6298b4133b
lxlx19@gmail.com <lxlx19@gmail.com>
2023-06-05 00:08:28 -0300

    keybinds

--- lua/core/plugins/luasnip.lua
+++ lua/core/plugins/luasnip.lua
@@ -16,13 +16,13 @@ return {
   -- stylua: ignore
   keys = {
     {
-      "<tab>",
+      "<F8>",
       function()
         return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
       end,
       expr = true, silent = true, mode = "i",
     },
-    { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-    { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    { "<F8>", function() require("luasnip").jump(1) end, mode = "s" },
+    { "<F9>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
   },
 }
